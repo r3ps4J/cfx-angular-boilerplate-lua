4b20c19217ee47bf47da13f2d52fb4eee8cef224
Jasper <40723838+r3ps4J@users.noreply.github.com>
2022-12-03 22:07:06 +0100

    Add message observables to listen for NUI messages

--- web/src/app/nui.service.ts
+++ web/src/app/nui.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from "@angular/core";
+import { fromEvent, Observable, Subject } from "rxjs";
+
+interface NuiMessage<T = any> {
+	action: string;
+	data: T;
+}
 
 @Injectable({
 	providedIn: "root",
@@ -8,24 +14,48 @@ export class NuiService {
 		? (window as any).GetParentResourceName()
 		: "nui-app";
 
-	constructor() {}
+	private messageObservable: Observable<MessageEvent>;
+	private actionObservables: Record<string, Subject<any>> = {};
 
-	isEnvBrowser(): boolean {
+	private lastMessages: Record<string, any> = {};
+
+	constructor() {
+		this.messageObservable = fromEvent<MessageEvent<NuiMessage>>(window, "message");
+		this.messageObservable.subscribe((event: MessageEvent<NuiMessage>) => {
+			this.lastMessages[event.data.action] = event.data;
+			if (this.actionObservables[event.data.action]) {
+				this.actionObservables[event.data.action].next(event.data.data);
+			}
+		});
+	}
+
+	public isEnvBrowser(): boolean {
 		return !(window as any).invokeNative;
 	}
-	
+
 	async fetchNui<T = any>(eventName: string, data?: any, mockData?: T): Promise<T> {
-        if (this.isEnvBrowser() && mockData) {
-            return mockData;
-        }
-        const options = {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json; charset=UTF-8",
-            },
-            body: JSON.stringify(data),
-        };
-        const response = await fetch(`https://${this.resourceName}/${eventName}`, options);
-        return await response.json();
-    }
+		if (this.isEnvBrowser() && mockData) {
+			return mockData;
+		}
+		const options = {
+			method: "post",
+			headers: {
+				"Content-Type": "application/json; charset=UTF-8",
+			},
+			body: JSON.stringify(data),
+		};
+		const response = await fetch(`https://${this.resourceName}/${eventName}`, options);
+		return await response.json();
+	}
+
+	public fromMessageAction<T = any>(action: string): Subject<T> {
+		if (!this.actionObservables[action]) {
+			this.actionObservables[action] = new Subject<T>();
+		}
+		return this.actionObservables[action];
+	}
+
+	public getLastMessageData<T = any>(action: string): T | false {
+		return this.lastMessages[action] ?? false;
+	}
 }
