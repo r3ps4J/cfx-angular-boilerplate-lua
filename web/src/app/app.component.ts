e4e71e1b57514b1082457f473081ee6d78d0fd8f
Jasper <40723838+r3ps4J@users.noreply.github.com>
2022-12-03 23:07:06 +0100

    Add visibility functionality

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NuiService } from "./nui.service";
 
 interface ReturnData {
@@ -12,12 +12,22 @@ interface ReturnData {
 	templateUrl: "./app.component.html",
 	styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	visible: boolean = false;
 	clientData?: ReturnData;
 
-	constructor(private nui: NuiService) {
+	constructor(private nui: NuiService) {}
+
+	ngOnInit(): void {
+		// This listens for the "setVisible" message
+		this.nui.fromMessageAction<boolean>("setVisible").subscribe({
+			next: (value) => {
+				this.visible = value;
+			}
+		});
+
 		// This will set the NUI to visible if we are developing in browser
-		nui.dispatchDebugMessage([
+		this.nui.dispatchDebugMessage([
 			{
 				action: "setVisible",
 				data: true
@@ -37,5 +47,5 @@ export class AppComponent {
 				console.error("Setting mock data due to error", e);
 				this.clientData = { x: 500, y: 300, z: 200 };
 			});
-	};
+	}
 }
