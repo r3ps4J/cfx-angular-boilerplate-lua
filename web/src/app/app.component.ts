eac44962147e9e7ff6139d57ba331f32b5211bda
Jasper <40723838+r3ps4J@users.noreply.github.com>
2022-12-03 22:57:43 +0100

    Add example app from react boilerplate

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from "@angular/core";
+import { NuiService } from "./nui.service";
+
+interface ReturnData {
+	x: number;
+	y: number;
+	z: number;
+}
 
 @Component({
 	selector: "app-root",
@@ -6,5 +13,29 @@ import { Component } from "@angular/core";
 	styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-	title = "web";
+	clientData?: ReturnData;
+
+	constructor(private nui: NuiService) {
+		// This will set the NUI to visible if we are developing in browser
+		nui.dispatchDebugMessage([
+			{
+				action: "setVisible",
+				data: true
+			}
+		]);
+	}
+
+	handleGetClientData() {
+		this.nui
+			.fetchNui<ReturnData>("getClientData")
+			.then((retData) => {
+				console.log("Got return data from client scripts:");
+				console.dir(retData);
+				this.clientData = retData;
+			})
+			.catch((e) => {
+				console.error("Setting mock data due to error", e);
+				this.clientData = { x: 500, y: 300, z: 200 };
+			});
+	};
 }
